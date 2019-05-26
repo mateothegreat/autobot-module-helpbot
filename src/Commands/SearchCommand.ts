ad49c4c406771ca5477afe3927c350a5aa621bcb
Matthew Davis <matthew@matthewdavis.io>
2019-05-25 19:39:38 -0500

    bump

--- src/Commands/SearchCommand.ts
+++ src/Commands/SearchCommand.ts
@@ -89,6 +89,9 @@ export class SearchCommand extends CommandBase {
 
         } else {
 
+            console.log(command.arguments);
+
+
             const results: Array<HelpBotQuestion> = await DB.connection.getRepository(HelpBotQuestion)
                                                             .find({
 
