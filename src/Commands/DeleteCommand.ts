fa4fd83fdd58f3cd3bcd262aee010a21b8bf09f4
Matthew Davis <matthew@matthewdavis.io>
2019-05-25 23:40:27 -0500

    bump

--- src/Commands/DeleteCommand.ts
+++ src/Commands/DeleteCommand.ts
@@ -2,7 +2,6 @@ import { Command, CommandBase, CommandParser, DB, Event, Logger } from '@autobot
 import { RichEmbed }                                              from 'discord.js';
 import { HelpBotQuestion }                                        from '../DB/HelpBotQuestion';
 import { HelpBotQuestionStatus }                                  from '../DB/HelpBotQuestionStatus';
-import { HelpBotTag }                                             from '../DB/HelpBotTag';
 
 /**
  * Search the HelpDesk questions.
@@ -49,10 +48,10 @@ export class DeleteCommand extends CommandBase {
         //
         // First we try to retrieve the tag by name.
         //
-        const result = await DB.connection.getRepository(HelpBotTag)
+        const result = await DB.connection.getRepository(HelpBotQuestion)
                                .createQueryBuilder('t')
                                .select([ '*' ])
-                               .where('name = :name', { name: command.namedarguments.name })
+                               .where('id = :id', { name: command.namedarguments.id })
                                .getRawOne();
 
         //
